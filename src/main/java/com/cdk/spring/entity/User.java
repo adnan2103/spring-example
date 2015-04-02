1d41c5ae00dcf51984582d0e8f84ba3a22503c81
Adnan Khan <Adnan2103@hotmail.com>
2015-04-02 12:24:04 +0530

    Adding projects file

---
+++ src/main/java/com/cdk/spring/entity/User.java
@@ -0,0 +1,39 @@
+package com.cdk.spring.entity;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Created by khana on 3/12/2015.
+ */
+
+@XmlRootElement
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    public User() {
+        // default constructor for hibernate.
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
---
+++ src/main/java/com/cdk/spring/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.cdk.spring.repository;
+
+import com.cdk.spring.entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by khana on 3/13/2015.
+ */
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
